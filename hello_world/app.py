767fbf72865d9ded0ed3b7f0571f890ec33a654a
Luiz Galvao <llgalvao24@gmail.com>
2022-11-09 02:05:37 -0300

    chore: update props and add hello-world endpoint

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,42 +1,12 @@
-import json
+from fastapi import FastAPI
+from mangum import Mangum
 
-# import requests
+app = FastAPI()
 
 
-def lambda_handler(event, context):
-    """Sample pure Lambda function
+@app.get("/")
+def read_root():
+    return {"Hello": "World", "ok": True}
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+lambda_handler = Mangum(app=app, lifespan="off")
