d2ac383fb4a8e0bb5573dc9e165cba9b5f7994b8
Luiz Galvao <llgalvao24@gmail.com>
2022-11-09 02:25:07 -0300

    chore: update main folder and func names

--- api/app.py
+++ api/app.py
@@ -9,4 +9,9 @@ def read_root():
     return {"Hello": "World", "ok": True}
 
 
+@app.get("/hello")
+def hello_mount():
+    return {"message": "All is well", "ok": True}
+
+
 lambda_handler = Mangum(app=app, lifespan="off")
