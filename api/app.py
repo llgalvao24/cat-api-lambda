a354c22afbaeda7494de62ec4b280763c2c522cb
Luiz Galvao <llgalvao24@gmail.com>
2022-11-10 02:06:08 -0300

    feat: create breed controller and refac app.py

--- api/app.py
+++ api/app.py
@@ -1,17 +1,10 @@
 from fastapi import FastAPI
 from mangum import Mangum
-import boto3
-from boto3.dynamodb.conditions import Key, Attr
-from model.breed import BreedModel
-from uuid import uuid4
+from controller import breed
 
 app = FastAPI()
 
-dynamo_resource = boto3.resource('dynamodb')
-
-print(list(dynamo_resource.tables.all()))
-
-breed_table = dynamo_resource.Table('breed')
+app.include_router(breed.router)
 
 
 @app.get("/")
@@ -22,31 +15,6 @@ def read_root():
 @app.get("/hello")
 def hello_mount():
     return {"message": "All is well", "ok": True}
-
-
-@app.post("/api/breed")
-def create_breed(Breed: BreedModel):
-    breed = dict(
-        breed_id=str(uuid4()),
-        name=Breed.name,
-        description=Breed.description,
-        life_span=Breed.life_span,
-        temperament=Breed.temperament,
-        origin=Breed.origin,
-    )
-
-    resp = breed_table.put_item(Item=breed)
-
-    if resp['ResponseMetadata']['HTTPStatusCode'] == 200:
-        return {"message": "Breed created successfully", "ok": True}
-
-    return {"message": "Breed creation failed", "ok": False}
-
-
-@app.get("/api/breeds")
-def get_breeds():
-    resp = breed_table.scan()
-    return resp['Items']
 
 
 lambda_handler = Mangum(app=app, lifespan="off")
---
+++ api/controller/breed.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter
+import boto3
+from uuid import uuid4
+from service.cat_api import fetch_breeds
+from model.breed import BreedModel
+
+router = APIRouter()
+dynamo_resource = boto3.resource('dynamodb')
+breed_table = dynamo_resource.Table('breed')
+
+
+@router.get("/api/fetch-breeds")
+def call_fetch_breeds():
+    return fetch_breeds()
+
+
+@router.post("/api/breed")
+def create_breed(Breed: BreedModel):
+    breed = BreedModel(
+        breed_id=str(uuid4()),
+        name=Breed.name,
+        description=Breed.description,
+        life_span=Breed.life_span,
+        temperament=Breed.temperament,
+        origin=Breed.origin
+    )
+
+    resp = breed_table.put_item(Item=breed)
+
+    if resp['ResponseMetadata']['HTTPStatusCode'] == 200:
+        return {"message": "Breed created successfully", "ok": True}
+
+    return {"message": "Breed creation failed", "ok": False}
+
+
+@router.get("/api/breeds")
+def get_breeds():
+    resp = breed_table.scan()
+    return resp['Items']
