9a24d60fe21a2efa4ac65f131d56dc04816d1e23
Luiz Galvao <llgalvao24@gmail.com>
2022-11-13 20:14:45 -0300

    feat: update breed model

--- api/models/breed.py
+++ api/models/breed.py
@@ -8,17 +8,3 @@ class BreedModel(BaseModel):
     life_span: str
     temperament: str
     origin: str
-
-    def __init__(self, breed_id, name, description, life_span, temperament, origin):
-        self.breed_id = breed_id
-        self.name = name
-        self.description = description
-        self.life_span = life_span
-        self.temperament = temperament
-        self.origin = origin
-
-    def __repr__(self):
-        return f"BreedModel(breed_id={self.breed_id}, \
-          name={self.name}, description={self.description}, \
-          life_span={self.life_span}, temperament={self.temperament}, \
-          origin={self.origin})"
